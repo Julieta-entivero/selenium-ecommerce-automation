3c1aacf7cfc088adc7936475ccca114ff294ae0e
Julieta Belen Entivero <julietabentivero@gmail.com>
2026-03-22 15:00:00 -0300

    agrego testng.xml y soporte headless para CI

--- src/test/java/com/shopzone/base/BaseTest.java
+++ src/test/java/com/shopzone/base/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
@@ -13,13 +15,23 @@ public class BaseTest {
     private static final String BASE_URL = "https://www.saucedemo.com/";
 
     @BeforeMethod
-    public void setUp() {
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
 
+        // para correr en CI o sin interfaz grafica
+        String headless = System.getProperty("headless");
+        if ("true".equals(headless)) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
         driver.get(BASE_URL);
     }
 
